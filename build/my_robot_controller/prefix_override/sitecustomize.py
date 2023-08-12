0fe42b05482c126c3b7c0b282198535766fa99b6
vaishnavi <vaishnavimulik25@gmail.com>
2023-08-12 18:31:53 +0530

    workspace created

---
+++ build/my_robot_controller/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/vaishnavi/ros2_ws/install/my_robot_controller'
---
+++ src/my_robot_controller/my_robot_controller/my_first_node.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import rclpy
+
+def main(args=None):
+        rclpy.init(args=args)
+
+
+if __name__ == '__main__':
+        
